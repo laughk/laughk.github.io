6cb1537caa122f00db5d31b6d36729a951a18dc8
Wercker <laughk.android+wercker@gmail.com>
2016-01-26 15:18:00 +0000

    Update by wercker

--- theme/js/popular-vote.js
+++
@@ -1,7 +0,0 @@
-Hatena.BookmarkWidget.url   = "http://memo.laughk.org";
-Hatena.BookmarkWidget.title = "エントリー";
-Hatena.BookmarkWidget.sort  = "hot";
-Hatena.BookmarkWidget.width = 0;
-Hatena.BookmarkWidget.num   = 5;
-Hatena.BookmarkWidget.theme = "notheme";
-Hatena.BookmarkWidget.load();
